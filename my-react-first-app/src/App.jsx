61573aaf6babc6256ec4e577b101d2f1e68f23a2
Yojjal Sharma <yojjal878@gmail.com>
2024-06-14 17:04:59 +0545

    new scrimba

--- my-react-first-app/src/App.jsx
+++ my-react-first-app/src/App.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import Navbar from './Navbar'
-import Main_content from './Main_content'
+
 
 export default function App(){
   return(
     <>
       <Navbar />
-      <Main_content />
-      
     </>
   )
 }
--- my-react-first-app/src/Main_content.jsx
+++
@@ -1,7 +0,0 @@
-export default function Main_content(){
-    return(
-        <>
-            <p>good</p>
-        </>
-    )
-}
