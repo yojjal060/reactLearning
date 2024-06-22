5d327cfc666fbefe628ca1e29bd6b424d215f433
Yojjal Sharma <yojjal878@gmail.com>
2024-06-23 00:09:14 +0545

    changing color using useEffect

--- useEffect/src/App.jsx
+++ useEffect/src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, {useState,useEffect} from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const[count,setCount] =  useState(0);
+
+  useEffect(()=>{
+    const color = count > 0 ? 'lightgreen' : count < 0 ? 'lightcoral' : 'white';
+    document.body.style.backgroundColor = color;
+  },[count])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Counter: {count}</h1>
+      <button onClick={()=>setCount(count + 1)}>Increment</button>
+      <button onClick={()=> setCount(count - 1)}>Decrement</button>
+      <p>count is: {count}</p>
     </>
-  )
+  );
 }
-
-export default App
