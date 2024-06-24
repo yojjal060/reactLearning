2d376b4bee4816e2dfbc0d4ef8ca51956bd05700
Yojjal Sharma <yojjal878@gmail.com>
2024-06-25 00:57:15 +0545

    added price div color to change dynamically with its price range

--- tour-newfeatures/src/App.jsx
+++ tour-newfeatures/src/App.jsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Card from './Card'; // Make sure this path matches your Card component file path
 
+
 export default function App() {
+
+  
+
+  
+
   return (
     <div className='container'>
       <div className='Title'>
@@ -14,24 +20,28 @@ export default function App() {
           image="https://www.course-api.com/images/tours/tour-1.jpeg "
           description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
           button
+          overlayText = "1,050"
         />
 
         <Card 
           image="https://www.course-api.com/images/tours/tour-2.jpeg"
           description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
           button
+          overlayText = "1,200"
         />
 
         <Card 
           image="https://www.course-api.com/images/tours/tour-3.jpeg "
           description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
           button
+          overlayText = "2,695"
         />
 
         <Card 
           image="https://www.course-api.com/images/tours/tour-4.jpeg "
           description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
           button
+          overlayText = "1,560"
         />
 
 
@@ -39,6 +49,7 @@ export default function App() {
           image="https://www.course-api.com/images/tours/tour-5.jpeg "
           description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
           button
+          overlayText = "3,050"
         />
 
 
--- tour-newfeatures/src/Card.jsx
+++ tour-newfeatures/src/Card.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import './Card.css'
 
-export default function Card({image,description}) {
+export default function Card({image,description,overlayText}) {
+  const [color, setColor] = useState("green");
+
+  useEffect(() => {
+    const price = parseFloat(overlayText.replace(/,/g, '')); // Remove commas and convert to number
+    if (price > 2000) {
+      setColor('red');
+    } else if(price > 1100) {
+      setColor('aqua');
+    }
+    else if(price < 1100){
+      setColor('green')
+    }
+  }, [overlayText]);
+  
+
   return (
     <div className='card'>
+      
       <img className='images' src={image} alt="image"/>
       <p>{description}</p>
       <button>Not Interested</button>
+      <div className="overlay-text" style={{ backgroundColor: color }}>{overlayText}</div>
     </div>
   )
 }
