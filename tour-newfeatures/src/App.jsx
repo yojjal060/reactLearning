c6027c83efeaee8f36f7603505d75c074ec24083
Yojjal Sharma <yojjal878@gmail.com>
2024-06-25 12:20:10 +0545

    price and card are responsive

--- tour-newfeatures/src/App.jsx
+++ tour-newfeatures/src/App.jsx
@@ -3,10 +3,10 @@ import './App.css';
 import Card from './Card'; // Make sure this path matches your Card component file path
 
 
+
 export default function App() {
 
   
-
   
 
   return (
@@ -21,6 +21,7 @@ export default function App() {
           description="Paris is synonymous with the finest things that culture can offer — in art, fashion, food, literature, and ideas. On this tour, your Paris-savvy Rick Steves guide will immerse you in the very best of the City of Light: the masterpiece-packed Louvre and Orsay museums, resilient Notre-Dame Cathedral, exquisite Sainte-Chapelle, and extravagant Palace of Versailles. You'll also enjoy guided neighborhood walks through the city's historic heart as well as quieter moments to slow down and savor the city's intimate cafés, colorful markets, and joie de vivre. Join us for the Best of Paris in 7 Days!"
           button
           overlayText = "1,050"
+          
         />
 
         <Card 
--- tour-newfeatures/src/Card.jsx
+++ tour-newfeatures/src/Card.jsx
@@ -1,38 +1,37 @@
-import React,{useState,useEffect} from 'react'
-import './Card.css'
+import React, { useState, useEffect } from 'react';
+import './Card.css';
 
-export default function Card({image,description,overlayText}) {
+export default function Card({ image, description, overlayText }) {
   const [color, setColor] = useState("green");
-  const [isExpanded,setIsExpanded] = useState(false)
+  const [isExpanded, setIsExpanded] = useState(false);
+  
 
   useEffect(() => {
     const price = parseFloat(overlayText.replace(/,/g, '')); // Remove commas and convert to number
     if (price > 2000) {
       setColor('red');
-    } else if(price > 1100) {
+    } else if (price > 1100) {
       setColor('aqua');
-    }
-    else if(price < 1100){
-      setColor('green')
+    } else if (price < 1100) {
+      setColor('green');
     }
   }, [overlayText]);
-  
+
   const toggleExpanded = () => {
     setIsExpanded(!isExpanded);
   };
 
   return (
-    <div className='card'>
-      
-      <img className='images' src={image} alt="image"/>
+    <div className={`card ${isExpanded ? 'expanded' : ''}`}>
+      <img className='images' src={image} alt="Tour" />
       <p>
         {isExpanded ? description : `${description.substring(0, 89)}...`}
-        <span className='see-more' style={{color: "#34d399"}} onClick={toggleExpanded}>
+        <span className='see-more' style={{ color: "#34d399" }} onClick={toggleExpanded}>
           {isExpanded ? '    See Less' : '   See More'}
         </span>
       </p>
       <button>Not Interested</button>
       <div className="overlay-text" style={{ backgroundColor: color }}>{overlayText}</div>
     </div>
-  )
+  );
 }
