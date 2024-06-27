a0b594cfc1117db2cd70f5ee4d6ee2a75a25f67c
Yojjal Sharma <yojjal878@gmail.com>
2024-06-27 17:58:44 +0545

    generic way to do not react way

---
+++ forms/src/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import Form from './assets/form'
+
+export default function App() {
+  return (
+    <><Form /></>
+  )
+}
---
+++ forms/src/assets/form.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Form() {
+  return (
+    <div>
+      <form action="GET">
+        <label htmlFor="input">Enter Your Name</label>
+        <input type="text" name="" id="input" />
+      </form>
+    </div>
+    
+  )
+}
