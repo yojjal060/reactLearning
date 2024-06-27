7344bd4fb345eff54bf9406a75fd7d40e56b924d
Yojjal Sharma <yojjal878@gmail.com>
2024-06-27 11:58:48 +0545

    div height using useref

--- useRef/src/App.jsx
+++ useRef/src/App.jsx
@@ -1,25 +1,34 @@
-import { useState, useEffect, useRef } from "react";
-
+import React ,{useState,useEffect,useRef} from 'react'
+import './App.css'
 
 export default function App() {
-  const [inputValue, setInputValue] = useState("");
-  const previousInputValue = useRef("");
+  let [div1Height,setDiv1HEight] = useState(0);
+  let [div2Height,setDiv2HEight] = useState(0);
+  let [div3Height,setDiv3HEight] = useState(0);
 
-  useEffect(() => {
-    previousInputValue.current = inputValue;
-  }, [inputValue]);
+  let div1Ref = useRef(null)
+  let div2Ref = useRef(null)
+  let div3Ref = useRef(null)
+
+  useEffect(()=>{
+    if(div1Ref.current){
+      setDiv1HEight(div1Ref.current.offsetHeight)
+    }
+    if(div2Ref.current){
+      setDiv2HEight(div2Ref.current.offsetHeight)
+    }
+    if(div3Ref.current){
+      setDiv3HEight(div3Ref.current.offsetHeight)
+    }
+  },[])
 
   return (
+    
     <>
-      <input
-        type="text"
-        value={inputValue}
-        onChange={(e) => setInputValue(e.target.value)}
-      />
-      <h2>Current Value: {inputValue}</h2>
-      <h2>Previous Value: {previousInputValue.current}</h2>
+    <div className="lol1" ref={div1Ref}>Div1 Height : {div1Height}</div>
+    <div className="lol2" ref={div2Ref}>Div2 Height : {div2Height}</div>
+    <div className="lol3" ref={div3Ref}>Div3 Height : {div3Height}</div>
+    
     </>
-  );
+  )
 }
-
-
