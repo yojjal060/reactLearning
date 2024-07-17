a7e659d1ef575f825a07e9fba3e1e9f9ce6735d3
Yojjal Sharma <yojjal878@gmail.com>
2024-07-17 23:33:31 +0545

    server added

--- forms/vite.config.js
+++ forms/vite.config.js
@@ -3,15 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
   plugins: [react()],
   server:{
     host:'0.0.0.0'
   }
-=======
-  server:{
-    host:'0.0.0.0'
-  },
-  plugins: [react()],
->>>>>>> 1a4a295214543cc1d534b29a2a08d7ce738eb255
 })
