3fbc01c76cd1020470c34083815b823f4f22340d
Yojjal Sharma <yojjal878@gmail.com>
2024-07-20 22:59:02 +0545

    yay

--- carousel/src/App.jsx
+++ carousel/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Content from './content'
+import slides from './assets/imagesData.json'
 
 export default function App() {
   return (
     <div>
-      <Content />
+      <Content data={slides} />
     </div>
   )
 }
--- carousel/src/content.jsx
+++ carousel/src/content.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
-export default function content() {
+
+export default function content({data}) {
   return (
-    <>
-     
-    </>
-  )
+    <div>
+      {data.slides.map((item,idx)=>{
+        return <img src={item.src} alt={item.alt} key={idx} />
+      })}
+    </div>
+  )//yay
 }
